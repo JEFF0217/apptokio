d1cee6aa6280cbe5e9143605a8c52bdf94c08c01
JEFF0217 <67935913+JEFF0217@users.noreply.github.com>
2021-07-29 10:10:31 -0500

    cambio db

--- src/datasources/proyectomovilesdb.datasource.ts
+++ src/datasources/proyectomovilesdb.datasource.ts
@@ -2,14 +2,14 @@ import {inject, lifeCycleObserver, LifeCycleObserver} from '@loopback/core';
 import {juggler} from '@loopback/repository';
 
 const config = {
-  name: 'clearDB_heroku',
+  name: 'ProyectoMoviles',
   connector: 'mysql',
   url: '',
-  host: 'us-cdbr-east-04.cleardb.com',
+  host: '34.133.182.181',
   port: 3306,
-  user: 'b77214896aa644',
-  password: '9d4842d9',
-  database: 'heroku_ac8dd84c7b5b059'
+  user: 'proyectomovilesdb',
+  password: 'proyectomovilesdb',
+  database: 'ProyectoMoviles'
 };
 
 // Observe application's life cycle to disconnect the datasource when
